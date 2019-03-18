ce88033bb1de443b514319e5ff668b4c0dfe4492
heqing01122 <782085017@qq.com>
2019-03-18 15:47:07 +0800

    123

--- zhibo8-web/src/main/resources/static/js_1/mindex.js
+++ zhibo8-web/src/main/resources/static/js_1/mindex.js
@@ -315,7 +315,7 @@
         if(flag){
             var html='';
             html+='<div class="box"><div class="box_bg"></div>';
-            html+='<div class="box_box"><div class="box_ent"><div class="box_center"><h2 class="box_t">选择你的属性</h2><ul><li><a href="javascript:;" value="basketball">只看篮球</a></li><li><a href="javascript:;" value="football">只看足球</a></li><li><a href="javascript:;" value="zhongyao">全部</a></li></ul></div></div></div></div>';
+            html+='<div class="box_box"><div class="box_ent"><div class="box_center"><h2 class="box_t">选择你的属性</h2><ul><li><a href="javascript:;" value="basketball">只看篮球</a></li><li><a href="javascript:;" value="football">只看足球</a></li><li><a href="javascript:;" value="zhongyao">重要</a></li></ul></div></div></div></div>';
             $(".global").append(html);
         }else{
             $(".global").find(".box").remove();
