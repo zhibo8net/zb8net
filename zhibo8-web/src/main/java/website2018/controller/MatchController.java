7b2caba78d9462635a04720dc39a1c8617720b93
baowei <314364805@qq.com>
2019-05-26 21:13:25 +0800

    代码提交

--- zhibo8-web/src/main/java/website2018/controller/MatchController.java
+++ zhibo8-web/src/main/java/website2018/controller/MatchController.java
@@ -88,48 +88,71 @@ public class MatchController extends BaseEndPoint {
     }
 
     @RequestMapping(value = "/live_play_inner/{id}")
-    public String live_play_inner(@PathVariable Long id, Model model,HttpServletRequest request) {
+    public String live_play_inner(@PathVariable Long id, Model model, HttpServletRequest request) {
         try {
             Live live = liveService.findById(id);
             if (live == null || live.match == null) {
                 return "redirect:http://www.zhibo8.net/";
             }
             Map<String, String> sysParamMap = CacheUtils.getSysMap();
-            String interfaceChange=sysParamMap.get("LIVE_ZHIBO_IS_INTERFACE_CHANAGE")==null?"":sysParamMap.get("LIVE_ZHIBO_IS_INTERFACE_CHANAGE");
-            String change=sysParamMap.get("LIVE_ZHIBO_IS_PLAY_CHANAGE")==null?"":sysParamMap.get("LIVE_ZHIBO_IS_PLAY_CHANAGE");
-            String CONTAIN_URL=sysParamMap.get("LIVE_ZHIBO_CONTAIN_URL")==null?"":sysParamMap.get("LIVE_ZHIBO_CONTAIN_URL");
+            String interfaceChange = sysParamMap.get("LIVE_ZHIBO_IS_INTERFACE_CHANAGE") == null ? "" : sysParamMap.get("LIVE_ZHIBO_IS_INTERFACE_CHANAGE");
+            String change = sysParamMap.get("LIVE_ZHIBO_IS_PLAY_CHANAGE") == null ? "" : sysParamMap.get("LIVE_ZHIBO_IS_PLAY_CHANAGE");
+            String CONTAIN_URL = sysParamMap.get("LIVE_ZHIBO_CONTAIN_URL") == null ? "" : sysParamMap.get("LIVE_ZHIBO_CONTAIN_URL");
+            String FILTERS_STREAM = sysParamMap.get("LIVE_ZHIBO_FILTERS_STREAM") == null ? "" : sysParamMap.get("LIVE_ZHIBO_FILTERS_STREAM");
+            String PLAY_CHANGE_IPHONE = sysParamMap.get("LIVE_ZHIBO_PLAY_CHANGE_IPHONE") == null ? "" : sysParamMap.get("LIVE_ZHIBO_PLAY_CHANGE_IPHONE");
+            String PLAY_CHANGE_PC = sysParamMap.get("LIVE_ZHIBO_PLAY_CHANGE_PC") == null ? "" : sysParamMap.get("LIVE_ZHIBO_PLAY_CHANGE_PC");
+            String PLAY_CHANGE_ANDRIOD = sysParamMap.get("LIVE_ZHIBO_PLAY_CHANGE_ANDRIOD") == null ? "" : sysParamMap.get("LIVE_ZHIBO_PLAY_CHANGE_ANDRIOD");
 
 
             LiveDTO liveDTO = BeanMapper.map(live, LiveDTO.class);
             liveDTO.link = live.link;
             model.addAttribute("liveDTO", liveDTO);
-            if(StringUtils.equals("TRUE", interfaceChange)){
-                liveDTO.link=keyUrlService.getKeyUrl(liveDTO.link);
+            if (StringUtils.equals("TRUE", interfaceChange)) {
+                liveDTO.link = keyUrlService.getKeyUrl(liveDTO.link);
             }
             if (StringUtils.isNotEmpty(liveDTO.link)) {
 
-            if(StringUtils.equals("TRUE", change)){
-                List<String> containList= Lists.newArrayList(CONTAIN_URL.split("\\|"));
-                boolean flag=false;
-                for(String str:containList){
-                    if(liveDTO.link.indexOf(str)>=0){
-                        flag=true;
-                        break;
+                if (StringUtils.equals("TRUE", change)) {
+                    List<String> containList = Lists.newArrayList(CONTAIN_URL.split("\\|"));
+                    boolean flag = false;
+                    for (String str : containList) {
+                        if (liveDTO.link.indexOf(str) >= 0) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag) {
+                        List<String> filtersList = Lists.newArrayList(FILTERS_STREAM.split("\\|"));
+
+                        String agent = request.getHeader("user-agent");
+
+                        boolean filters = false;
+                        for (String str : filtersList) {
+                            if (liveDTO.link.indexOf(str) >= 0) {
+                                filters = true;
+                                break;
+                            }
+                        }
+                        if (!filters) {
+                            if (agent.contains("iPhone") || agent.contains("iPod") || agent.contains("iPad")) {
+                                if(StringUtils.equals(PLAY_CHANGE_IPHONE,"TRUE")){
+                                    liveDTO.link = liveDTO.link.replace("m3u8", "flv");
+                                }
+
+                            } else if (agent.contains("Android") || agent.contains("android")) {
+                                if(StringUtils.equals(PLAY_CHANGE_ANDRIOD,"TRUE")){
+                                    liveDTO.link = liveDTO.link.replace("m3u8", "flv");
+                                }
+                            } else {
+                                if(StringUtils.equals(PLAY_CHANGE_PC,"TRUE")){
+                                    liveDTO.link =liveDTO.link.replace("m3u8", "flv");
+                                }
+                            }
+                        }
                     }
                 }
-                if(flag){
 
-                    String agent= request.getHeader("user-agent");
 
-                    if(agent.contains("iPhone")||agent.contains("iPod")||agent.contains("iPad")){
-
-                    }else{
-                        liveDTO.link= liveDTO.link.replace("m3u8","flv");
-                    }
-
-                }
-
-            }
             }
             MatchDTO matchDTO = indexService.findMatchDTO(live.match.id);
             if (matchDTO == null) {
